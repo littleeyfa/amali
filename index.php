7ba021f10634c94b8e25b39e82ad04794c240c64
littleeyfa <160107997+littleeyfa@users.noreply.github.com>
2024-02-18 23:25:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,15 @@
 
 <h1>Source Code Management</h1>
 <p>Semester 4</p>
-
+<tr>
+  <th>Firstname</th>
+  <th>Lastname</th>
+  <th>Umur</th>
+</tr>
+<tr>
+  <td>Eyfa</td>
+  <td>Elyana</td>
+  <td>20</td>
+</tr>
 </body>
 </html>
